966cd0929b3f33f035f0a65e88ebb17c52ce5d4a
snbnpython <167688978+snbnpython@users.noreply.github.com>
2024-06-07 23:11:16 +0900

    アップロード

---
+++ kadai_005/bmi.js
@@ -0,0 +1,11 @@
+// 変数の宣言・値の代入
+let userWeight = 68;
+
+// 変数の宣言・値の代入
+let userHeight = 1.7;
+
+// 変数の宣言・値の代入
+let userBmi = userWeight / (userHeight * userHeight)
+
+// コンソールへの出力
+console.log(userBmi);
